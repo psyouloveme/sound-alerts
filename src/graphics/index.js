044e3b2cea96d81144df25ac1f6c22fa96813797
psyouloveme <4607641+psyouloveme@users.noreply.github.com>
2022-06-11 15:32:36 -0700

    Remove unneeded nightbot commands

--- src/graphics/index.js
+++ src/graphics/index.js
@@ -1,22 +1,12 @@
 const NIGHTBOT_ALERTS = [
-	{
-		audioCmd: '!blackmail',
-		text: 'don\'t you know that blackmail is waaaayyy uncool'
-	},
-	{audioCmd: '!key', text: 'A key, but to what?'},
 	{audioCmd: '!ticket2', text: 'I need to pick up my ticket first.'},
-	{
-		audioCmd: '!ticket1',
-		text: 'I should get my ticket before going to the harbor...'
-	},
-	{audioCmd: '!ticket3', text: 'I should get the ticket first.'},
-	{audioCmd: '!wind', text: 'Just the wind?'}
+	{audioCmd: '!ticket1', text: 'I should get my ticket before going to the harbor...'},
+	{audioCmd: '!ticket3', text: 'I should get the ticket first.'}
 ];
 
 const audioEnabled = nodecg.Replicant('audio_alerts_enabled');
 const onChatReceived = ({user = '', message = ''}) => {
 	if (!audioEnabled.value || !user || !message || (user !== 'nightbot' && !message.startsWith('!'))) {
-		console.log('exiting early');
 		return;
 	}
 
