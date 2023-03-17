8ad113f8621b72ef14cd715a9a89b135a796beea
psyouloveme <4607641+psyouloveme@users.noreply.github.com>
2023-03-17 01:49:39 -0700

    Fix replicant errors, update graphic logic

--- src/graphics/index.js
+++ src/graphics/index.js
@@ -1,30 +1,25 @@
-const NIGHTBOT_ALERTS = [
-	{audioCmd: '!ticket2', text: 'I need to pick up my ticket first.'},
-	{audioCmd: '!ticket1', text: 'I should get my ticket before going to the harbor...'},
-	{audioCmd: '!ticket3', text: 'I should get the ticket first.'}
-];
+const audioEnabled = nodecg.Replicant('audio_alerts_enabled', {
+	defaultValue: false
+});
 
-const audioEnabled = nodecg.Replicant('audio_alerts_enabled');
 const onChatReceived = ({user = '', message = ''}) => {
-	if (!audioEnabled.value || !user || !message || (user !== 'nightbot' && !message.startsWith('!'))) {
+	if (!user || !message || !message.startsWith('!')) {
 		return;
 	}
 
 	const splitAlert = message.split(' ')[0];
 	if (splitAlert && nodecg.findCue(splitAlert)) {
 		nodecg.playSound(splitAlert);
-	} else if (user.toLowerCase() === 'nightbot') {
-		let sendCmd = '';
-		for (let i = 0; i < NIGHTBOT_ALERTS.length && !sendCmd; i++) {
-			if (NIGHTBOT_ALERTS[i].text === message.trim()) {
-				sendCmd = NIGHTBOT_ALERTS[i].audioCmd;
-			}
-		}
-
-		if (sendCmd && nodecg.findCue(sendCmd)) {
-			nodecg.playSound(sendCmd);
-		}
 	}
 };
 
-nodecg.listenFor('ChatReceived', 'twitch-listener', onChatReceived);
+NodeCG.waitForReplicants(audioEnabled).then(() => {
+	audioEnabled.on('change', (nextVal, prevVal) => {
+		if (!prevVal && nextVal) {
+			nodecg.listenFor('ChatReceived', 'twitch-listener', onChatReceived);
+		} else if (prevVal && !nextVal) {
+			nodecg.unlisten('ChatReceived', 'twitch-listener', onChatReceived);
+		}
+	});
+});
+
