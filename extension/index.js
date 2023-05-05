529765969e2a5e30ebf3c9c2a56a9f47f4ca4025
psyouloveme <4607641+psyouloveme@users.noreply.github.com>
2023-05-05 02:09:44 -0700

    Update binaries

--- extension/index.js
+++ extension/index.js
@@ -1,2 +1,2 @@
-module.exports=function(e){e.log.debug("Soundalerts bundle started."),e.Replicant("audio_alerts_enabled",{defaultValue:!1});const l=e.Replicant("audio-alert-log",{defaultValue:{}});e.listenFor("played-cue",((a,u)=>{a&&(a in l.value?l.value[a]+=1:l.value[a]=1,e.log.debug("cue %s is now at %s plays",a,l.value[a])),u&&!u.handled&&u(null)}))};
+module.exports=function(e){e.log.debug("Soundalerts bundle started."),e.Replicant("audio_alerts_enabled",{defaultValue:!1});const l=e.Replicant("audio-alert-log",{defaultValue:{}});l.value={},e.listenFor("played-cue",((a,u)=>{a&&(a in l.value?l.value[a]+=1:l.value[a]=1,e.log.debug("cue %s is now at %s plays",a,l.value[a])),u&&!u.handled&&u(null)}))};
 //# sourceMappingURL=index.js.map
