bc864d1836047320d2e5e857f242ddc762302571
psyouloveme <4607641+psyouloveme@users.noreply.github.com>
2023-05-11 00:48:33 -0700

    temp

--- src/dashboard/settings/types.d.ts
+++ src/dashboard/settings/types.d.ts
@@ -1,25 +1,25 @@
 
 export enum CSSClasses {
-    commandFormRow = "commandFormRow",
-    btnRemove = "btnRemove",
-    btnAdd = "btnAdd",
-    formGroup = "formGroup",
-    middle = "middle",
-    btnToggleEnabled = "btnToggleEnabled",
-    enabled = "enabled",
-    readOnly = "readOnly",
-    hidden = "hidden",
-    cueSelectWrapper = "cueSelectWrapper"
+  commandFormRow = 'commandFormRow',
+  btnRemove = 'btnRemove',
+  btnAdd = 'btnAdd',
+  formGroup = 'formGroup',
+  middle = 'middle',
+  btnToggleEnabled = 'btnToggleEnabled',
+  enabled = 'enabled',
+  readOnly = 'readOnly',
+  hidden = 'hidden',
+  cueSelectWrapper = 'cueSelectWrapper'
 };
 
 export enum ElementIDs {
-    cueConfigPanel = "cueConfigPanel"
+  cueConfigPanel = 'cueConfigPanel'
 };
 
-export type ConfigFormState = {
-    editingRows: number[],
-    submittingRows: number[],
-    rowErrors: Record<number, string>,
-    rendered: boolean,
-    commandTypeOptions: SoundCommandType[]
+export interface ConfigFormState {
+  editingRows: number[]
+  submittingRows: number[]
+  rowErrors: Record<number, string>
+  rendered: boolean
+  commandTypeOptions: SoundCommandType[]
 }
--- src/extension/ChatListeners.ts
+++ src/extension/ChatListeners.ts
@@ -1,94 +1,95 @@
-import nodecgHandle from "./NodeCGHandle";
+import { type SoundCommandEvents } from '../types/SoundCommandEvents'
+import nodecgHandle from './NodeCGHandle'
 
 // old method
-function onChatReceived({ user = "", message = "" }) {
-	if (!user || !message || !message.startsWith("!")) {
-	  return;
-	}
+function onChatReceived ({ user = '', message = '' }): void {
+  if (!user || !message || !message.startsWith('!')) {
+	  return
+  }
 
-	const splitAlert = message.split(" ")[0];
-	if (splitAlert && nodecg.findCue(splitAlert)) {
-	  nodecg.playSound(splitAlert);
-	  nodecg.sendMessage(SoundCommandEvents.playedSoundCue, splitAlert);
-	}
+  const splitAlert = message.split(' ')[0]
+  if ((splitAlert.length > 0) && (nodecg.findCue(splitAlert) != null)) {
+	  nodecg.playSound(splitAlert)
+	  void nodecg.sendMessage(SoundCommandEvents.playedSoundCue, splitAlert)
+  }
 };
 
-export function twitchChatReceived ({ user = "", message = "" }) {
-	if (!user || !message) {
-		return;
-	}
-	if (!nodecgHandle.current) {
-		throw Error("Cue command change failed: NodeCG handle is invalid.");
-	}
-	const nodecg = nodecgHandle.current;
+export function twitchChatReceived ({ user = '', message = '' }) {
+  if (!user || !message) {
+    return
+  }
+  if (nodecgHandle.current == null) {
+    throw Error('Cue command change failed: NodeCG handle is invalid.')
+  }
+  const nodecg = nodecgHandle.current
 
-	const soundCueMap = nodecg.Replicant<SoundCommandList>(SoundAlertReplicants.soundCueConfig);
-	if (soundCueMap.value === undefined) {
-		nodecg.log.error(`Cue command change failed: Sound cue map is undefined.`);
-		return;
-	}
+  const soundCueMap = nodecg.Replicant<SoundCommandList>(SoundAlertReplicants.soundCueConfig)
+  if (soundCueMap.value === undefined) {
+    nodecg.log.error('Cue command change failed: Sound cue map is undefined.')
+    return
+  }
 
-	const splitAlert = message.split(" ")[0];
-	let idx = soundCueMap.value.findIndex((c) => c.commandName === splitAlert);
-	if (idx < 0) {
-		return;
-	}
-	const cmd = soundCueMap.value[idx];
-	if (!cmd.enabled) {
-		nodecg.log.debug(`Cue command ${splitAlert} is disabled, not playing.`);
-	}
-	if (!cmd.allCuesAreValid) {
-		nodecg.log.warn(`Cue command ${splitAlert} has invalid cues, not playing.`);
-		return;
-	}
-	if (cmd.coolDownMs && cmd.lastUseTimestamp && (Date.now() - cmd.lastUseTimestamp) < cmd.coolDownMs) {
-		nodecg.log.warn(`Cue command ${splitAlert} is still on cooldown, not playing.`);
-		return;
-	}
-	if (!cmd.mappedCues || cmd.mappedCues.length === 0) {
-		nodecg.log.warn(`Cue command ${splitAlert} has no mapped cues.`);
-		return;
-	}
+  const splitAlert = message.split(' ')[0]
+  const idx = soundCueMap.value.findIndex((c) => c.commandName === splitAlert)
+  if (idx < 0) {
+    return
+  }
+  const cmd = soundCueMap.value[idx]
+  if (!cmd.enabled) {
+    nodecg.log.debug(`Cue command ${splitAlert} is disabled, not playing.`)
+  }
+  if (!cmd.allCuesAreValid) {
+    nodecg.log.warn(`Cue command ${splitAlert} has invalid cues, not playing.`)
+    return
+  }
+  if (cmd.coolDownMs && cmd.lastUseTimestamp && (Date.now() - cmd.lastUseTimestamp) < cmd.coolDownMs) {
+    nodecg.log.warn(`Cue command ${splitAlert} is still on cooldown, not playing.`)
+    return
+  }
+  if (!cmd.mappedCues || cmd.mappedCues.length === 0) {
+    nodecg.log.warn(`Cue command ${splitAlert} has no mapped cues.`)
+    return
+  }
 
-	let cue : string | null = null;
-	let orderedIndex = cmd.orderedMappingIndex || 0;
-	if (cmd.mappedCues.length < orderedIndex) {
-		orderedIndex = 0;
-	}
-	let nextOrderedIndex = null;
-	
-	switch (cmd.commandType) {
-		case SoundCommandType.single:
-			cue = cmd.mappedCues[0];
-			break;
-		case SoundCommandType.ordered:
-			cue = cmd.mappedCues[orderedIndex];
-			nextOrderedIndex = orderedIndex + 1;
-			break;
-		case SoundCommandType.random:
-			cue = cmd.mappedCues[Math.floor(Math.random() * cmd.mappedCues.length)];
-			break;
-		default:
-			nodecg.log.warn(`Cue command ${splitAlert} has an invalid type.`);
-			return;
-	}
+  let cue: string | null = null
+  let orderedIndex = cmd.orderedMappingIndex || 0
+  if (cmd.mappedCues.length < orderedIndex) {
+    orderedIndex = 0
+  }
+  let nextOrderedIndex = null
 
-	const knownSoundCues = nodecg.Replicant<SoundCueNameList>(SoundAlertReplicants.soundCueList);
-	if (soundCueMap.value === undefined ) {
-		nodecg.log.error(`Cue command change failed: Sound cue list is undefined.`);
-		return;
-	}
+  switch (cmd.commandType) {
+    case SoundCommandType.single:
+      cue = cmd.mappedCues[0]
+      break
+    case SoundCommandType.ordered:
+      cue = cmd.mappedCues[orderedIndex]
+      nextOrderedIndex = orderedIndex + 1
+      break
+    case SoundCommandType.random:
+      cue = cmd.mappedCues[Math.floor(Math.random() * cmd.mappedCues.length)]
+      break
+    default:
+      nodecg.log.warn(`Cue command ${splitAlert} has an invalid type.`)
+      return
+  }
 
-	if (!cue || !knownSoundCues.value.find((c) => c === cue)) {
-		nodecg.log.warn(`Cue command ${splitAlert} did not map to a known cue. Mapped cue was ${cue || '"null"'}.`);
-		return;
-	}
+  const knownSoundCues = nodecg.Replicant<SoundCueNameList>(SoundAlertReplicants.soundCueList)
+  if (soundCueMap.value === undefined) {
+    nodecg.log.error('Cue command change failed: Sound cue list is undefined.')
+    return
+  }
 
-	nodecg.sendMessage(SoundCommandEvents.playSoundCue, cue);
-	soundCueMap.value[idx] = {
-		...soundCueMap.value[idx],
-		lastUseTimestamp: Date.now(),
-		orderedMappingIndex: nextOrderedIndex,
-		commandUsageCount: soundCueMap.value[idx].commandUsageCount + 1
-	}
+  if (!cue || !knownSoundCues.value.find((c) => c === cue)) {
+    nodecg.log.warn(`Cue command ${splitAlert} did not map to a known cue. Mapped cue was ${cue || '"null"'}.`)
+    return
+  }
+
+  nodecg.sendMessage(SoundCommandEvents.playSoundCue, cue)
+  soundCueMap.value[idx] = {
+    ...soundCueMap.value[idx],
+    lastUseTimestamp: Date.now(),
+    orderedMappingIndex: nextOrderedIndex,
+    commandUsageCount: soundCueMap.value[idx].commandUsageCount + 1
+  }
 }
